28f591ef9dde64d4b0b73312b79d42818091feb2
Masiaka8 <masiaka8@outlook.com>
2023-02-07 18:14:27 +0300

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+import pages.*;
+import java.util.concurrent.TimeUnit;
+
+@Listeners(TestListener.class)
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected LoginPage loginPage;
+    protected ProductsPage productsPage;
+    protected ShoppingCartPage shoppingCartPage;
+    protected ProductDetailsPage productsDetailPage;
+    protected CheckoutOnePage checkoutOnePage;
+    protected CheckoutTwoPage checkoutTwoPage;
+
+    @Parameters ({"browser"})
+    @BeforeClass(alwaysRun = true)
+    public void setUp(@Optional("chrome") ITestContext testContext, String browserName) throws Exception {
+        if(browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if(browserName.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new Exception("Incorrect browser name");
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        testContext.setAttribute("driver", driver);
+
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        shoppingCartPage = new ShoppingCartPage(driver);
+        productsDetailPage = new ProductDetailsPage(driver);
+        checkoutOnePage = new CheckoutOnePage(driver);
+        checkoutTwoPage = new CheckoutTwoPage(driver);
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void  navigate() {
+        driver.get("http://prestashop.qatestlab.com.ua/en/");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void  tearDown() {
+        driver.quit();
+    }
+}

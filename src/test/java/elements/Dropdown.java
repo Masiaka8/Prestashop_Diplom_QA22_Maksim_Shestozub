65d1cd38eeb6c21b5dd31cd0a9e9b90064159b05
Masiaka8 <masiaka8@outlook.com>
2023-03-02 07:46:10 +0300

    Commit 3.1

--- src/test/java/elements/Dropdown.java
+++ src/test/java/elements/Dropdown.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 
 public class Dropdown extends BaseElement{
 
-    private final static String DROPDOWN_LOCATOR = "#%s";
     private final static String OPTION_DROPDOWN_LOCATOR = "//*[@id='id_state']/option[text()='%s']";
 
     public Dropdown(WebDriver driver, String label) {
@@ -14,7 +13,7 @@ public class Dropdown extends BaseElement{
     }
 
     public void selectDropdownValue(String optionName)  {
-        WebElement dropdownButton = driver.findElement(By.cssSelector(String.format(DROPDOWN_LOCATOR, this.label)));
+        WebElement dropdownButton = driver.findElement(By.id(label));
         scrollIntoView(dropdownButton);
         dropdownButton.click();
         driver.findElement(By.xpath(String.format(OPTION_DROPDOWN_LOCATOR, optionName))).click();
--- src/test/java/elements/Input.java
+++ src/test/java/elements/Input.java
@@ -1,23 +1,18 @@
 package elements;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import java.util.Objects;
 
-
 public class Input extends BaseElement {
-    private final static String INPUT_LOCATOR = "#%s";
 
     public Input(WebDriver driver, String label) {
         super(driver, label);
     }
 
     public void setValue(String value) {
-        WebElement inputElement = driver.findElement(By.cssSelector(String.format(INPUT_LOCATOR, label)));
+        WebElement inputElement = driver.findElement(By.id(label));
         if (Objects.nonNull(value)) {
             scrollIntoView(inputElement);
             inputElement.sendKeys(value);
